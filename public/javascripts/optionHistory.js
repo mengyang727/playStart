e92461c5901d4a341fd06b7f47be9cec822ca6e5
mengyang727 <mengyang727@126.com>
2018-02-27 08:46:21 +0800

    datepicker

--- public/javascripts/optionHistory.js
+++ public/javascripts/optionHistory.js
@@ -1,13 +1,4 @@
 $(
-//function() {
-//    $.getJSON("api/v1/accounts/", function(json) {
-//        var table = $("#p021SspPubSiteHistoryList");
-//        for (var i = 0; i < json.length; i++) {
-//            table.append("<tr><td>" + json[i].id + "</td><td>" + json[i].name + "</td></tr>");
-//        }
-//    });
-//}
-
 function() {
 
 
